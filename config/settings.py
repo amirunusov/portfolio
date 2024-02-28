0be12b98eb5a47cb293c6b96b4df2b9e8cd48daf
amirunusov <135022478+amirunusov@users.noreply.github.com>
2024-02-28 15:14:54 +0500

    cacawc

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mkv-e#w)4sk!v++6803o^h#c)o@b$y%&5a$rvidyilmqk*$ogx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.82.193.224', 'localhost', '127.0.0.1:8000', 'sub.amrk.uz']
+ALLOWED_HOSTS = ['3.82.193.224', 'localhost', '127.0.0.1:8000', 'config.amrk.uz']
 
 
 # Application definition
